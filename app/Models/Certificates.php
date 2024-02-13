5d3e008fe9374edacfec06de5dc01644c915f98e
Sainjurohan <Sainju34@gmail.com>
2024-02-13 11:40:29 +0545

    feat: update Certificate Model and update package incorporating simplesoftwareio/simple-qrcode for qr-code generation

--- app/Models/Certificates.php
+++ app/Models/Certificates.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Certificates extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'employee_name',
+        'qr_code',
+        'certificate_status',
+        'pdf_file_path',
+    ];
+
 }
